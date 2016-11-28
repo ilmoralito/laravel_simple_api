e23a7c51aedaacabff12bebdf6b8b8e1ed16372c
Mario Martinez <amakenadog@gmail.com>
2016-11-28 15:43:52 -0600

    Displaying cusomert profile and country when instance transactions

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -10,7 +10,7 @@ class TransactionController extends Controller
 {
     public function index($date1, $date2)
     {
-        return Transaction::with('customer')
+        return Transaction::with(['customer.profile', 'customer.country'])
             ->whereBetween('date', [$date1, $date2])
             ->get()
             ->toArray();
